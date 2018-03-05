d186e7d7bf05a1cedb580fc605791c33c49a17f1
Amandine Latour <amandine.latour+github@gmail.com>
2018-03-05 15:36:55 -0500

    fixed create method

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -2,18 +2,18 @@ class StationsController < ApplicationController
   include StationsHelper
 
   def index
-    create_stations if stations_need_create_or_update?
-    update_available_bikes if bixis_need_update?
-    set_stations_variables
+    update_and_set_stations_variables
   end
 
   def create
-    set_stations_variables
+    update_and_set_stations_variables
   end
 
   private
 
-  def set_stations_variables
+  def update_and_set_stations_variables
+    create_stations if stations_need_create_or_update?
+    update_available_bikes if bixis_need_update?
     @stations_with_bixis = Station.where("available_bixis > 0").order(:distance_from_office).limit(5)
     @closest_stations = Station.order(:distance_from_office).limit(5) if @stations_with_bixis.empty?
   end
